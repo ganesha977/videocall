0b0c86c68f9faf26976ffbc8c7960d07c6c58d90
Rahulkallur823 <rr1189183@gmail.com>
2024-11-07 20:50:22 +0530

    ready for deployment

--- frontend/src/environment.js
+++ frontend/src/environment.js
@@ -1,6 +1,8 @@
-const server = 
 
-    "http://localhost:8000"
+
+let IS_PROD=true;
+
+const server = IS_PROD?"https://videocall-3vv7.onrender.com":"http://localhost:8000"
 
 
 export default server;
